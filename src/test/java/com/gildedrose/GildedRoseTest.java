a1cdbb780678e806fca6591ffee3a46d4abe1551
gaetano-dst <gaetano.dst@emfazis.be>
2019-09-19 23:20:45 +0200

    [KATA-GDS] Copy project from GiledRose.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,14 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
+    private static final String FOO_PRODUCT_NAME = "foo";
+
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void updateQuality_does_alter_item_name() {
+        Item[] items = new Item[] { new Item(FOO_PRODUCT_NAME, 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(FOO_PRODUCT_NAME, app.items[0].name);
     }
 
 }
